f246843974299962bc8537e743b9cf37229a70a8
Bharath-NB <Bharath.s8499@gmail.com>
2024-12-20 18:03:44 +0530

    first commit

---
+++ src/Components/Dashboard.jsx
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Dashboard = () => {
+    const navigate = useNavigate()
+    axios.defaults.withCredentials = true;
+    useEffect(() => {
+        axios.get('https://password-reset-backend-ra1t.onrender.com/auth/verify')
+        .then(res=> {
+            if(res.data.status) {
+
+            } else {
+                navigate('/')
+            }
+            console.log(res)
+        })
+    }, [])
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
